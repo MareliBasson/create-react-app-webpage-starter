dfddead8120e3f71aea7d9f42891b51c761eae02
Mareli <ambra.lioness@gmail.com>
2019-07-05 12:00:05 +0200

    submenu tab bug
    the small triangle tab at the top of the submenu disappeared when adding transition effects instead of using display; block  as usual to show a submenu on hover.

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,23 +1,35 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
-import "./menu.css";
+import React, { Component } from "react"
+import { NavLink } from "react-router-dom"
+import "./menu.css"
 
 class Menu extends Component {
-  render() {
-    return (
-      <div className="rw-page-menu">
-        <NavLink activeClassName="active" exact to="/">
-          Home
-        </NavLink>
-        <NavLink activeClassName="active" to="/page1">
-          Page 1
-        </NavLink>
-        <NavLink activeClassName="active" to="/page2">
-          Page 2
-        </NavLink>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="rw-page-menu">
+				<NavLink activeClassName="active" exact to="/">
+					Home
+				</NavLink>
+				<NavLink activeClassName="active" to="/page1">
+					Page 1
+				</NavLink>
+				<NavLink activeClassName="active" to="/page2">
+					Page 2
+				</NavLink>
+				<a href="">
+					<div className="menu-label">Test</div>
+					<div className="sub-menu">
+						<ul>
+							<li>t1</li>
+							<li>t2</li>
+							<li>t3</li>
+							<li>t4</li>
+							<li>t5</li>
+						</ul>
+					</div>
+				</a>
+			</div>
+		)
+	}
 }
 
-export default Menu;
+export default Menu
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Routes from "routes.js";
-import registerServiceWorker from "./registerServiceWorker";
+import React from "react"
+import ReactDOM from "react-dom"
+import Routes from "routes.js"
+import registerServiceWorker from "./registerServiceWorker"
 
 // Stylesheets
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap-theme.css";
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap-theme.css"
+import "./index.css"
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
-registerServiceWorker();
+ReactDOM.render(<Routes />, document.getElementById("root"))
+registerServiceWorker()
